13810ed941ca4748070975c221b70cc412ca9839
The-EleetCoder <jaijain1803@gmail.com>
2024-09-14 13:34:47 +0530

    integrated socket.io

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "public/index.html"));
+});
+
+// socket : like a client
+// io : like a server
+io.on("connection", (socket) => {
+  console.log("a user connected: ", socket.id);
+  socket.on("user-message", (message) => {
+    console.log("a new message from user: ", message);
+    io.emit("message", message);
+  });
 });
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log("server running at http://localhost:3000");
 });
